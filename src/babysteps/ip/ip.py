0d1438deacc44c0aef5abf6da096703a11d0cb12
Craig Hollabaugh <craig@hollabaugh.com>
2019-04-20 06:33:13 +0100

    added exception handling if interface has no ip address

--- src/babysteps/ip/ip.py
+++ src/babysteps/ip/ip.py
@@ -16,8 +16,14 @@ def get_ip_address(ifname):
         struct.pack('256s', ifname[:15])
     )[20:24])
 
-eth0 = get_ip_address('eth0') 
-wlan0 = get_ip_address('wlan0') 
+try:
+    eth0 = get_ip_address('eth0') 
+except IOError:
+    eth0 = "n/a"
+try:
+    wlan0 = get_ip_address('wlan0') 
+except IOError:
+    wlan0 = "n/a"
 
 i2cbus = SMBus(1)        
 oled = ssd1306(i2cbus)   
@@ -27,9 +33,10 @@ oled.canvas.rectangle((0, 0, oled.width-1, oled.height-1), outline=1, fill=0)
 
 # Write two lines of text.
 oled.canvas.text((5,5),    'eth0', fill=1)
-oled.canvas.text((5,15),    eth0, fill=1)
+oled.canvas.text((10,15),    eth0, fill=1)
 oled.canvas.text((5,25),   'wlan0', fill=1)
-oled.canvas.text((5,35),    wlan0, fill=1)
+oled.canvas.text((10,35),    wlan0, fill=1)
+oled.display()
 
 print " eth0:",eth0
 print "wlan0:",wlan0
