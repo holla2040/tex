413316ce256c5d95f1df49680ff76c3529737e9d
Craig Hollabaugh <craig@hollabaugh.com>
2019-05-02 02:44:32 -0600

    first working version

--- src/babysteps/websockets/app.py
+++ src/babysteps/websockets/app.py
@@ -5,28 +5,24 @@ from flask_socketio import SocketIO
 import RPi.GPIO as gpio
 import time, threading
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app,async_mode='threading')
 
 red      = 11 
 green    = 9
 blue     = 8
+
 s1	 = 22
 s2	 = 10
-
-s1last   = 0
-s2last   = 0
 
 gpio.setmode(gpio.BCM)
 gpio.setup(red  ,gpio.OUT)
 gpio.setup(green,gpio.OUT)
 gpio.setup(blue, gpio.OUT)
-gpio.setmode(gpio.BCM)
+
 gpio.setup(s1,   gpio.IN)
 gpio.setup(s2,   gpio.IN)
-
 
 @app.route("/")
 def index():
@@ -50,25 +46,20 @@ def tex_event(json):
     except:
         pass
 
+@socketio.on('connect')
+def connect():
+    socketio.emit('tex',{'data':{'s1':gpio.input(s1)}}) 
+    socketio.emit('tex',{'data':{'s2':gpio.input(s2)}}) 
+
 def rpiloop():
     s1last = 0
     s2last = 0
     while True:
-        s1state = gpio.input(s1)
-        if s1state != s1last:
-            socketio.emit('tex',{'data':{'s1':s1state}}) 
-            print("s1 diff")
-            s1last = s1state
-        s2state = gpio.input(s2)
-        if s2state != s2last:
-            socketio.emit('tex',{'data':{'s2':s2state}}) 
-            print("s2 diff")
-            s2last = s2state
-        time.sleep(0.1)
+        time.sleep(0.5)
+        socketio.emit('tex',{'data':{'time':time.strftime("%y%m%d-%H%M%S",time.localtime(time.time())),'s1':gpio.input(s1),'s2':gpio.input(s2)}}) 
 
 def flaskloop():
-    socketio.run(app,host='0.0.0.0',debug=True)
-
+    socketio.run(app,host='0.0.0.0')
 
 if __name__ == '__main__':
     threading.Thread(target=flaskloop).start()
