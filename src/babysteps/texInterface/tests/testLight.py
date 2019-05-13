c54cfdff1ff526b9421045e142e4f68df32f7c7d
Alfred Lorber <Alfred@TheLorbers.com>
2019-05-12 23:00:01 -0600

    "testLight.py: Tweeks, additions, etc"

--- src/babysteps/texInterface/tests/testLight.py
+++ src/babysteps/texInterface/tests/testLight.py
@@ -6,10 +6,10 @@ import os
 sys.path.insert(0,os.path.join(filePath, ".."))
 
 import unittest, time
-from texInterface import TexInterface
 from lights import Light
+from texInterface import TexInterface
 
-class TestLightsBase(unittest.TestCase):
+class TestLightBase(unittest.TestCase):
     def setUp(self):
         #print ("In setUp()")
         self.texInterface = TexInterface()
@@ -26,7 +26,7 @@ class TestLightsBase(unittest.TestCase):
     def tearDownClass(cls):
         pass
 
-class TestLight(TestLightsBase):
+class TestLight(TestLightBase):
     def testLightInit(self):
         light = Light()
         self.assertEqual(light.texInterface, None,"Checking light.texInterface")
@@ -37,142 +37,149 @@ class TestLight(TestLightsBase):
 
     def testLightSetup(self):
         light = Light()
-        light.setup(self.texInterface, 0, TexInterface.LIGHT_LEVEL.ON*.8)
+        light.setup(self.texInterface, 0, Light.LEVEL.HIGH*.8)
         self.assertEqual(light.texInterface, self.texInterface,
                          "Checking light.texInterface")
         self.assertEqual(light.ledNumber   , 0,
                          "Checking light.ledNumber   ")
-        self.assertEqual(light.onLightLevel, TexInterface.LIGHT_LEVEL.ON*.8,
+        self.assertEqual(light.onLightLevel, Light.LEVEL.HIGH*.8,
                          "Checking light.onLightLevel")
-        self.assertEqual(light.lightLevel  , TexInterface.LIGHT_LEVEL.OFF,
+        self.assertEqual(light.lightLevel  , Light.LEVEL.LOW,
                          "Checking light.lightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_OFF,
                          "Checking light.lightMode   ")
+        light.cleanup()
 
     def testLightOff(self):
         light = Light()
-        light.setup(self.texInterface, 0, TexInterface.LIGHT_LEVEL.ON*.8)
+        light.setup(self.texInterface, 0, Light.LEVEL.HIGH*.8)
         light.off()
-        self.assertEqual(light.lightLevel  , TexInterface.LIGHT_LEVEL.OFF,
+        self.assertEqual(light.lightLevel  , Light.LEVEL.LOW,
                          "Checking light.lightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_OFF,
                          "Checking light.lightMode   ")
+        light.cleanup()
 
     def testLightOn(self):
         light = Light()
-        light.setup(self.texInterface, 0, TexInterface.LIGHT_LEVEL.ON*.8)
+        light.setup(self.texInterface, 0, Light.LEVEL.HIGH*.8)
         light.on()
-        self.assertEqual(light.lightLevel  , TexInterface.LIGHT_LEVEL.ON*.8,
+        self.assertEqual(light.lightLevel  , Light.LEVEL.HIGH*.8,
                          "Checking light.lightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_ON,
                          "Checking light.lightMode   ")
+        light.cleanup()
 
     def testLightToggle(self):
         light = Light()
-        light.setup(self.texInterface, 0, TexInterface.LIGHT_LEVEL.ON*.8)
+        light.setup(self.texInterface, 0, Light.LEVEL.HIGH*.8)
         light.on()
-        self.assertEqual(light.lightLevel  , TexInterface.LIGHT_LEVEL.ON*.8,
+        self.assertEqual(light.lightLevel  , Light.LEVEL.HIGH*.8,
                          "Checking light.lightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_ON,
                          "Checking light.lightMode   ")
         light.toggle() # Turn off
-        self.assertEqual(light.lightLevel  , TexInterface.LIGHT_LEVEL.OFF,
+        self.assertEqual(light.lightLevel  , Light.LEVEL.LOW,
                          "Checking light.lightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_OFF,
                          "Checking light.lightMode   ")
         light.toggle() # Turn on
-        self.assertEqual(light.lightLevel  , TexInterface.LIGHT_LEVEL.ON*.8,
+        self.assertEqual(light.lightLevel  , Light.LEVEL.HIGH*.8,
                          "Checking light.lightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_ON,
                          "Checking light.lightMode   ")
         light.toggle() # Turn off
-        self.assertEqual(light.lightLevel  , TexInterface.LIGHT_LEVEL.OFF,
+        self.assertEqual(light.lightLevel  , Light.LEVEL.LOW,
                          "Checking light.lightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_OFF,
                          "Checking light.lightMode   ")
+        light.cleanup()
 
     def testLightSetOnLightLevel(self):
         light = Light()
-        light.setup(self.texInterface, 0, TexInterface.LIGHT_LEVEL.ON*.8)
-        light.setOnLightLevel(TexInterface.LIGHT_LEVEL.ON*.2)
-        self.assertEqual(light.lightLevel  , TexInterface.LIGHT_LEVEL.OFF,
+        light.setup(self.texInterface, 0, Light.LEVEL.HIGH*.8)
+        light.setOnLightLevel(Light.LEVEL.HIGH*.2)
+        self.assertEqual(light.lightLevel  , Light.LEVEL.LOW,
                          "Checking light.lightLevel  ")
-        self.assertEqual(light.onLightLevel, TexInterface.LIGHT_LEVEL.ON*.2,
+        self.assertEqual(light.onLightLevel, Light.LEVEL.HIGH*.2,
                          "Checking light.onLightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_OFF,
                          "Checking light.lightMode   ")
         light.on()
-        self.assertEqual(light.lightLevel  , TexInterface.LIGHT_LEVEL.ON*.2,
+        self.assertEqual(light.lightLevel  , Light.LEVEL.HIGH*.2,
                          "Checking light.lightLevel  ")
-        self.assertEqual(light.onLightLevel, TexInterface.LIGHT_LEVEL.ON*.2,
+        self.assertEqual(light.onLightLevel, Light.LEVEL.HIGH*.2,
                          "Checking light.onLightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_ON,
                          "Checking light.lightMode   ")
-        light.setOnLightLevel(TexInterface.LIGHT_LEVEL.ON*.4)
-        self.assertEqual(light.lightLevel  , TexInterface.LIGHT_LEVEL.ON*.4,
+        light.setOnLightLevel(Light.LEVEL.HIGH*.4)
+        self.assertEqual(light.lightLevel  , Light.LEVEL.HIGH*.4,
                          "Checking light.lightLevel  ")
-        self.assertEqual(light.onLightLevel , TexInterface.LIGHT_LEVEL.ON*.4,
+        self.assertEqual(light.onLightLevel , Light.LEVEL.HIGH*.4,
                          "Checking light.onLightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_ON,
                          "Checking light.lightMode   ")
         light.off()
-        light.setOnLightLevel(TexInterface.LIGHT_LEVEL.ON*.6)
-        self.assertEqual(light.lightLevel  , TexInterface.LIGHT_LEVEL.OFF,
+        light.setOnLightLevel(Light.LEVEL.HIGH*.6)
+        self.assertEqual(light.lightLevel  , Light.LEVEL.LOW,
                          "Checking light.lightLevel  ")
-        self.assertEqual(light.onLightLevel , TexInterface.LIGHT_LEVEL.ON*.6,
+        self.assertEqual(light.onLightLevel , Light.LEVEL.HIGH*.6,
                          "Checking light.onLightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_OFF,
                          "Checking light.lightMode   ")
+        light.cleanup()
 
     def testIncrementOnLightLevel(self):
         light = Light()
-        light.setup(self.texInterface, 0, TexInterface.LIGHT_LEVEL.ON*.8)
-        light.incrementOnLightLevel(-TexInterface.LIGHT_LEVEL.ON*.2)
-        self.assertLess(abs(light.lightLevel-TexInterface.LIGHT_LEVEL.OFF),1e-6,
+        light.setup(self.texInterface, 0, Light.LEVEL.HIGH*.8)
+        light.incrementOnLightLevel(-Light.LEVEL.HIGH*.2)
+        self.assertLess(abs(light.lightLevel-Light.LEVEL.LOW),1e-6,
                                "Checking light.lightLevel  ")
-        self.assertLess(abs(light.onLightLevel-TexInterface.LIGHT_LEVEL.ON*.6),1e-6,
+        self.assertLess(abs(light.onLightLevel-Light.LEVEL.HIGH*.6),1e-6,
                         "Checking light.onLightLevel")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_OFF,
                          "Checking light.lightMode   ")
         light.on()
-        self.assertLess(abs(light.lightLevel-TexInterface.LIGHT_LEVEL.ON*.6),1e-6,
+        self.assertLess(abs(light.lightLevel-Light.LEVEL.HIGH*.6),1e-6,
                         "Checking light.lightLevel  ")
-        self.assertLess(abs(light.onLightLevel-TexInterface.LIGHT_LEVEL.ON*.6),1e-6,
+        self.assertLess(abs(light.onLightLevel-Light.LEVEL.HIGH*.6),1e-6,
                                "Checking light.onLightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_ON,
                          "Checking light.lightMode   ")
-        light.incrementOnLightLevel(-TexInterface.LIGHT_LEVEL.ON*.2)
-        self.assertLess(abs(light.lightLevel-TexInterface.LIGHT_LEVEL.ON*.4),1e-6,
+        light.incrementOnLightLevel(-Light.LEVEL.HIGH*.2)
+        self.assertLess(abs(light.lightLevel-Light.LEVEL.HIGH*.4),1e-6,
                                "Checking light.lightLevel  ")
-        self.assertLess(abs(light.onLightLevel-TexInterface.LIGHT_LEVEL.ON*.4),1e-6,
+        self.assertLess(abs(light.onLightLevel-Light.LEVEL.HIGH*.4),1e-6,
                                "Checking light.onLightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_ON,
                          "Checking light.lightMode   ")
-        light.incrementOnLightLevel(-TexInterface.LIGHT_LEVEL.ON)
+        light.incrementOnLightLevel(-Light.LEVEL.HIGH)
         self.assertLess(abs(light.lightLevel-0),1e-6,
                                "Checking light.lightLevel  ")
         self.assertLess(abs(light.onLightLevel-0),1e-6,
                                "Checking light.onLightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_ON,
                          "Checking light.lightMode   ")
-        light.incrementOnLightLevel(TexInterface.LIGHT_LEVEL.ON*2)
-        self.assertLess(abs(light.lightLevel-TexInterface.LIGHT_LEVEL.ON),1e-6,
+        light.incrementOnLightLevel(Light.LEVEL.HIGH*2)
+        self.assertLess(abs(light.lightLevel-Light.LEVEL.HIGH),1e-6,
                                "Checking light.lightLevel  ")
-        self.assertLess(abs(light.onLightLevel-TexInterface.LIGHT_LEVEL.ON),1e-6,
+        self.assertLess(abs(light.onLightLevel-Light.LEVEL.HIGH),1e-6,
                                "Checking light.onLightLevel  ")
         self.assertEqual(light.lightMode   , Light.MODE.LIGHT_ON,
                          "Checking light.lightMode   ")
         light.off()
+        light.cleanup()
 
     def testGetLightMode(self):
         light = Light()
         self.assertEqual(light.getLightMode(), None)
-        light.setup(self.texInterface, 0, TexInterface.LIGHT_LEVEL.ON*.8)
+        light.setup(self.texInterface, 0, Light.LEVEL.HIGH*.8)
         self.assertEqual(light.getLightMode(), Light.MODE.LIGHT_OFF)
         light.on()
         self.assertEqual(light.getLightMode(), Light.MODE.LIGHT_ON)
         light.off()
         self.assertEqual(light.getLightMode(), Light.MODE.LIGHT_OFF)
+        light.cleanup()
 
         
 
@@ -213,6 +220,7 @@ class TestLight(TestLightsBase):
                                 '%s counting down' % (channel))
         for light in lightArray:
             light.off()
+            light.cleanup()
         
 if __name__ == '__main__':
     unittest.main()
